609282f3d4bb474bdd940667105f03fd2ab3c7e4
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-14 13:51:10 -0600

    moved main.py, connected to db, added message.js

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,14 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "storage": {
+    "test6DB": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
+    }
+  }
+}
