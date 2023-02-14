9fd31d7ad12ee12c96289ec7eab6d50fe5a4f22c
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-14 13:39:10 -0600

    added main.py

---
+++ src/components/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import json
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+from pydantic import BaseModel
+
+
+app = FastAPI()
+dynamodb = boto3.resource('dynamodb', region_name="us-east-2",
+         aws_access_key_id="AKIAX3FHVXYSJO3DLKG3",
+         aws_secret_access_key= "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")
+table = dynamodb.Table('memorextest-dev')
+
+
+
+# response = table.query(
+#     KeyConditionExpression=Key('PK').eq('COMP#0')
+# )
+
+response = table.get_item(
+   Key={
+      'PK': '0',
+      'SK': '0'
+   }
+)
+
+data = str(response['Item']['companyEmail'])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="http://localhost:3001/",
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["Content-Type","application/xml"],
+)
+
+@app.get("/")
+async def root():
+    return data
